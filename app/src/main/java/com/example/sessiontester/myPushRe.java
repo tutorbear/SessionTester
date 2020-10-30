7c5c5297e1981a8338701a9193e37af00fc7211f
tutorbear <tutorbeargroup@gmail.com>
2020-10-30 14:19:39 +0600

    push tester

--- app/src/main/java/com/example/sessiontester/myPushRe.java
+++ app/src/main/java/com/example/sessiontester/myPushRe.java
@@ -1,32 +1,11 @@
 package com.example.sessiontester;
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
-import com.parse.ManifestInfo;
-import com.parse.PLog;
 import com.parse.ParsePushBroadcastReceiver;
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Locale;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class myPushRe extends ParsePushBroadcastReceiver {
@@ -34,6 +13,6 @@ public class myPushRe extends ParsePushBroadcastReceiver {
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected NotificationChannel getNotificationChannel(Context context, Intent intent) {
-        return new NotificationChannel("parse_push", "General Alerts", NotificationManager.IMPORTANCE_HIGH);
+        return new NotificationChannel("my push", "General Books", NotificationManager.IMPORTANCE_HIGH);
     }
 }
